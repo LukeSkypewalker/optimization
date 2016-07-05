2fb509a643d991eeb196ff5d80f268f1ec031cf7
LukeSkypewalker <work37@gmail.com>
2016-07-05 10:27:46 +0300

    key refactored

--- key_layout/bigpams_processor.py
+++ key_layout/bigpams_processor.py
@@ -1,18 +1,17 @@
 def calc_combs_sums(left_hand_combs, bigrams):
-    comb_sum = []
-    for left_hand_comb in left_hand_combs:
-        sum_freq = calc_sum_freq(bigrams, left_hand_comb)
-        comb_sum.append([left_hand_comb, sum_freq])
-    return comb_sum
+    return [(comb, calc_sum_freq(bigrams, comb)) for comb in left_hand_combs]
 
 
 def calc_sum_freq(bigrams, left_hand_comb):
-    sum_prob = 0
-    for bigram in bigrams:
-        if (bigram[0] in left_hand_comb) != (bigram[1] in left_hand_comb):
-            sum_prob += bigram[2]
-    return sum_prob
+    return sum(b[2] for b in bigrams if (b[0] in left_hand_comb) != (b[1] in left_hand_comb))
 
 
+if __name__ == '__main__':
+    from itertools import combinations
+    from key_layout.data import *
 
+    left_hand_layout_combs = combinations(letters8, len(letters8) // 2)
+    comb_sum = calc_combs_sums(left_hand_layout_combs, bigrams)
+    comb_sum.sort(key=lambda comb: comb[1])
+    print(*comb_sum, sep='\n')
 
--- key_layout/letters_processor.py
+++ key_layout/letters_processor.py
@@ -1,34 +1,24 @@
-def calc_combs_sums(letters, bigrams):
-    data = []
-    for letter in letters:
-        sum_freq = calc_sum_freq(letter, bigrams)
-        data.append([letter, sum_freq])
-    return data
+def calc_letters_freq(letters, bigrams):
+    return [(letter, calc_letter_freq_sum(letter, bigrams)) for letter in letters]
 
 
-def calc_sum_freq(letter, bigrams):
-    sum_prob = 0
-    for bigram in bigrams:
-        if letter in (bigram[0], bigram[1]):
-            sum_prob += bigram[2]
-    return sum_prob
+def calc_letter_freq_sum(letter, bigrams):
+    return sum(b[2] for b in bigrams if letter in (b[0], b[1]))
+    # return sum(b[2] for b in calc_letter_bigrams_freq(letter, bigrams))
 
 
-def calc_letters_freq(letter, bigrams):
-    data = []
-    for bigram in bigrams:
-        if letter in (bigram[0], bigram[1]):
-            data.append(bigram)
-    return data
+def calc_letter_bigrams_freq(letter, bigrams):
+    return [b for b in bigrams if letter in (b[0], b[1])]
 
 
 if __name__ == '__main__':
     from key_layout.data import *
 
-    # letters_freq = calc_combs_sums(letters, bigrams)
-    # letters_freq.sort(key=lambda x: x[1], reverse=True)
-    # print(*letters_freq, sep='\n')
+    letters_freq = calc_letters_freq(letters26, bigrams)
+    letters_freq.sort(key=lambda x: x[1], reverse=True)
+    print(*letters_freq, sep='\n')
 
-    data = calc_letters_freq('B', bigrams)
-    print(*data, sep='\n')
+    # data = [calc_letter_freq(letter, bigrams) for letter in letters26]
+    # print(*data, sep='\n')
 
+    # print(*(b[2] for b in calc_letter_freq('A', bigrams)), sep='\n')
